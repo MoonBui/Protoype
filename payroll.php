76ed29a0a01fae14f6568ffa52f965da003baab4
Moon Bui <buinm@miamioh.edu>
2023-05-03 23:24:09 -0400

    adding test payroll page

---
+++ payroll.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <section class="curved-background">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css?version=0.0.1" type="text/css">
+            <title>Payroll</title>
+        </head>
+        <body>
+            <table>
+                <tr>
+                    <th>Hours Worked</th>
+                    <th>Salary</th>
+                    <th>Payment Date</th>
+                </tr>
+                <tr>
+                    <td>0.0</td>
+                    <td>0.0</td>
+                    <td>Some date</td>
+                </tr>
+            </table>
+        </body>
+    </section>
+</html>
